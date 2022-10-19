c4fd9e811305132d698ff22c387744e745a9e22a
nijatbakhtiyar@gmail.com <NijatBakhtiyar>
2022-10-20 01:41:58 +0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+});
